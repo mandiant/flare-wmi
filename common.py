805e848190f7466ed9b2df45eb148c6542d2a9c2
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-04-26 12:25:08 -0400

    common.py: massively improve performance by not inspecting the stack if the LoggingObject logging level is not enabled.

--- common.py
+++ common.py
@@ -24,15 +24,17 @@ class LoggingObject(object):
         return [self._getCallerFunction() + ": " + args[0]] + [a for a in args[1:]]
 
     def d(self, *args, **kwargs):
-        self._logger.debug(*self._formatFormatString(args), **kwargs)
+        if self._logger.isEnabledFor(logging.DEBUG):
+            self._logger.debug(*self._formatFormatString(args), **kwargs)
 
     def i(self, *args, **kwargs):
-        self._logger.info(*self._formatFormatString(args), **kwargs)
+        if self._logger.isEnabledFor(logging.INFO):
+            self._logger.info(*self._formatFormatString(args), **kwargs)
 
     def w(self, *args, **kwargs):
-        self._logger.warn(*self._formatFormatString(args), **kwargs)
+        if self._logger.isEnabledFor(logging.WARN):
+            self._logger.warn(*self._formatFormatString(args), **kwargs)
 
     def e(self, *args, **kwargs):
-        self._logger.error(*self._formatFormatString(args), **kwargs)
-
-
+        if self._logger.isEnabledFor(logging.ERROR):
+            self._logger.error(*self._formatFormatString(args), **kwargs)
