6c9edffd32706097d7eba8814d306ea1d997b25a
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-05-14 13:37:06 -0400

    dump_class_definition.py: fix bugs after pep8 updates

--- dump_class_definition.py
+++ dump_class_definition.py
@@ -3,75 +3,34 @@ import traceback
 
 from cim import CIM
 from cim import Index
-from cim import DATA_PAGE_SIZE
-from objects import CimContext
-from objects import get_class_id
-from objects import ClassLayout
+from objects import ObjectResolver
 from objects import CIM_TYPE_SIZES
-from objects import ClassDefinition
-from objects import QueryBuilderMixin
-from objects import ObjectFetcherMixin
 from common import h
-from common import one
-from common import LoggingObject
-
-
-def formatKey(k):
-    ret = []
-    for part in str(k).split("/"):
-        if "." in part:
-            ret.append(part[:7] + "..." + part.partition(".")[2])
-        else:
-            ret.append(part[:7])
-    return "/".join(ret)
-
-
-class Querier(LoggingObject, QueryBuilderMixin, ObjectFetcherMixin):
-    def __init__(self, context):
-        super(Querier, self).__init__()
-        self.context = context
-
-    def __repr__(self):
-        return "Querier()"
-
-    def get_class_definition(self, namespace, classname):
-        classId = get_class_id(namespace, classname)
-        cd = self.context.cdcache.get(classId, None)
-        if cd is None:
-            self.d("cdcache miss")
-            buf = self.get_class_definition_buffer(namespace, classname)
-            cd = ClassDefinition(buf)
-            self.context.cdcache[classId] = cd
-        return cd
-
-    def getClassLayout(self, namespace, classname):
-        cd = self.get_class_definition(namespace, classname)
-        return ClassLayout(self.context, namespace, cd)
 
 
 def dump_class_def(cd, cl):
-    print("classname: %s" % cd.class_name())
-    print("super: %s" % cd.super_class_name())
-    print("ts: %s" % cd.timestamp().isoformat("T"))
+    print("classname: %s" % cd.class_name)
+    print("super: %s" % cd.super_class_name)
+    print("ts: %s" % cd.timestamp.isoformat("T"))
     print("qualifiers:")
-    for k, v in cd.qualifiers().iteritems():
+    for k, v in cd.qualifiers.iteritems():
         print("  %s: %s" % (k, str(v)))
     print("properties:")
-    for propname, prop in cd.properties().iteritems():
-        print("  name: %s" % prop.name())
-        print("    type: %s" % prop.type())
-        print("    order: %s" % prop.entry_number())
+    for propname, prop in cd.properties.iteritems():
+        print("  name: %s" % prop.name)
+        print("    type: %s" % prop.type)
+        print("    order: %s" % prop.entry_number)
         print("    qualifiers:")
-        for k, v in prop.qualifiers().iteritems():
+        for k, v in prop.qualifiers.iteritems():
             print("      %s: %s" % (k, str(v)))
     print("layout:")
     off = 0
     for prop in cl.properties:
-        print("  (%s)   %s %s" % (h(off), prop.type(), prop.name()))
-        if prop.type().is_array():
+        print("  (%s)   %s %s" % (h(off), prop.type, prop.name))
+        if prop.type.is_array:
             off += 0x4
         else:
-            off += CIM_TYPE_SIZES[prop.type().type()]
+            off += CIM_TYPE_SIZES[prop.type.type]
 
 
 def main(type_, path, namespaceName, className):
@@ -79,23 +38,20 @@ def main(type_, path, namespaceName, className):
         raise RuntimeError("Invalid mapping type: {:s}".format(type_))
 
     c = CIM(type_, path)
-    ctx = CimContext(
-            c,
-            Index(c.getCimType(), c.logical_index_store()),
-            {}, {})
-    q = Querier(ctx)
+    i = Index(c.cim_type, c.logical_index_store)
+    o = ObjectResolver(c, i)
 
     while className != "":
         print("%s" % "=" * 80)
         print("namespace: %s" % namespaceName)
-        cd = q.get_class_definition(namespaceName, className)
-        cl = q.getClassLayout(namespaceName, className)
+        cd = o.get_cd(namespaceName, className)
+        cl = o.get_cl(namespaceName, className)
         try:
             dump_class_def(cd, cl)
         except:
             print("ERROR: failed to dump class definition!")
             print traceback.format_exc()
-        className = cd.super_class_name()
+        className = cd.super_class_name
 
 
 if __name__ == "__main__":
