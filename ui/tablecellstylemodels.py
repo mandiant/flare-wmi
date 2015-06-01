fc63ca425a608e219f4dd0483a832acb6cd4e00c
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-05-31 23:03:46 -0400

    hexview: move cell style models into new file

---
+++ ui/tablecellstylemodels.py
@@ -0,0 +1,209 @@
+from collections import namedtuple
+from collections import defaultdict
+
+from intervaltree import IntervalTree
+from PyQt5.QtCore import QObject
+from PyQt5.QtCore import pyqtSignal
+
+import os.path, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+from mutablenamedtuple import mutablenamedtuple
+from ui.colortheme import SolarizedColorTheme
+
+
+def row_start_index(index):
+    """ get index of the start of the 0x10 byte row containing the given index """
+    return index - (index % 0x10)
+
+
+def row_end_index(index):
+    """ get index of the end of the 0x10 byte row containing the given index """
+    return index - (index % 0x10) + 0xF
+
+
+def row_number(index):
+    """ get row number of the 0x10 byte row containing the given index """
+    return index / 0x10
+
+
+def column_number(index):
+    return index % 0x10
+
+
+ColoredRange = namedtuple("ColorRange", ["begin", "end", "color"])
+
+
+class ColorModel(QObject):
+    rangeChanged = pyqtSignal([ColoredRange])
+
+    def __init__(self, parent, color_theme=SolarizedColorTheme):
+        super(ColorModel, self).__init__(parent)
+        self._db = IntervalTree()
+        self._theme = color_theme
+
+    def color_region(self, begin, end, color=None):
+        if color is None:
+            color = self._theme.get_accent(len(self._db))
+        range = ColoredRange(begin, end, color)
+        self.color_range(range)
+        return range
+
+    def clear_region(self, begin, end):
+        span = end - begin
+        to_remove = []
+        for range in self._db[begin:end]:
+            if range.end - range.begin == span:
+                to_remove.append(range)
+        for range in to_remove:
+            self.clear_range(range.data)
+
+    def color_range(self, range):
+        self._db.addi(range.begin, range.end, range)
+        self.rangeChanged.emit(range)
+
+    def clear_range(self, range):
+        self._db.removei(range.begin, range.end, range)
+        self.rangeChanged.emit(range)
+
+    def get_color(self, index):
+        # ranges is a (potentially empty) list of intervaltree.Interval instances
+        # we sort them here from shorted length to longest, because we want
+        #    the most specific color
+        ranges = sorted(self._db[index], key=lambda r: r.end - r.begin)
+        if len(ranges) > 0:
+            return ranges[0].data.color
+        return None
+
+    def is_index_colored(self, index):
+        return len(self._db[index]) > 0
+
+    def is_region_colored(self, begin, end):
+        span = end - begin
+        for range in self._db[begin:end]:
+            if range.end - range.begin == span:
+                return True
+        return False
+
+
+ROLE_BORDER = 0xF
+BorderTheme = namedtuple("BorderTheme", ["color"])
+BorderData = namedtuple("BorderData", ["top", "bottom", "left", "right", "theme"])
+BorderedRange = namedtuple("BorderedRange", ["begin", "end", "theme", "cells"])
+
+
+CellT = mutablenamedtuple("CellT", ["top", "bottom", "left", "right"])
+def Cell(top=False, bottom=False, left=False, right=False):
+    return CellT(top, bottom, left, right)
+
+
+def compute_region_border(start, end):
+    # TODO: doc
+    cells = defaultdict(Cell)
+
+    start_row = row_number(start)
+    end_row = row_number(end)
+
+    ## topmost cells
+    if start_row == end_row:
+        for i in xrange(start, end):
+            cells[i].top = True
+    else:
+        for i in xrange(start, row_end_index(start) + 1):
+            cells[i].top = True
+    # cells on second row, top left
+    if start_row != end_row:
+        next_row_start = row_start_index(start) + 0x10
+        for i in xrange(next_row_start, next_row_start + column_number(start)):
+            cells[i].top = True
+
+    ## bottommost cells
+    if start_row == end_row:
+        for i in xrange(start, end):
+            cells[i].bottom = True
+    else:
+        for i in xrange(row_start_index(end), end):
+            cells[i].bottom = True
+    # cells on second-to-last row, bottom right
+    if start_row != end_row:
+        prev_row_end = row_end_index(end) - 0x10
+        for i in xrange(prev_row_end - (0x10 - column_number(end) - 1), prev_row_end + 1):
+            cells[i].bottom = True
+
+    ## leftmost cells
+    if start_row == end_row:
+        cells[start].left = True
+    else:
+        second_row_start = row_start_index(start) + 0x10
+        for i in xrange(second_row_start, row_start_index(end) + 0x10, 0x10):
+            cells[i].left = True
+    # cells in first row, top left
+    if start_row != end_row:
+        cells[start].left = True
+
+    ## rightmost cells
+    if start_row == end_row:
+        cells[end - 1].right = True
+    else:
+        penultimate_row_end = row_end_index(end) - 0x10
+        for i in xrange(row_end_index(start), penultimate_row_end + 0x10, 0x10):
+            cells[i].right = True
+    # cells in last row, bottom right
+    if start_row != end_row:
+        cells[end - 1].right = True
+
+    # convert back to standard dict
+    # trick from: http://stackoverflow.com/a/20428703/87207
+    cells.default_factory = None
+    return cells
+
+
+class BorderModel(QObject):
+    rangeChanged = pyqtSignal([BorderedRange])
+
+    def __init__(self, parent, color_theme=SolarizedColorTheme):
+        super(BorderModel, self).__init__(parent)
+        self._db = IntervalTree()
+        self._theme = color_theme
+
+    def border_region(self, begin, end, color=None):
+        if color is None:
+            color = self._theme.get_accent(len(self._db))
+        range = BorderedRange(begin, end, BorderTheme(color), compute_region_border(begin, end))
+        # note we use (end + 1) to ensure the entire selection gets captured
+        self._db.addi(range.begin, range.end + 1, range)
+        self.rangeChanged.emit(range)
+
+    def clear_region(self, begin, end):
+        span = end - begin
+        to_remove = []
+        for range in self._db[begin:end]:
+            if range.end - range.begin - 1 == span:
+                to_remove.append(range)
+        for range in to_remove:
+            self._db.removei(range.begin, range.end, range.data)
+            self.rangeChanged.emit(range.data)
+
+    def get_border(self, index):
+        # ranges is a (potentially empty) list of intervaltree.Interval instances
+        # we sort them here from shorted length to longest, because we want
+        #    the most specific border
+        ranges = sorted(self._db[index], key=lambda r: r.end - r.begin)
+        if len(ranges) > 0:
+            range = ranges[0].data
+            cell = range.cells.get(index, None)
+            if cell is None:
+                return None
+            ret = BorderData(cell.top, cell.bottom, cell.left, cell.right, range.theme)
+            return ret
+        return None
+
+    def is_index_bordered(self, index):
+        return len(self._db[index]) > 0
+
+    def is_region_bordered(self, begin, end):
+        span = end - begin
+        for range in self._db[begin:end]:
+            if range.end - range.begin == span:
+                return True
+        return False
+
