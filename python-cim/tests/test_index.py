4d22890a844c12705cffe070d6c1b1e171ef8e5a
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-01 18:02:12 -0400

    add test for index method

---
+++ python-cim/tests/test_index.py
@@ -0,0 +1,27 @@
+import collections
+
+import intervaltree
+
+import cim
+from fixtures import *
+
+
+def test_root(repo):
+    '''
+    Args:
+        repo (cim.CIM): the deleted-instance repo
+
+    Returns:
+        None
+    '''
+
+    # collected empirically
+    assert repo.logical_index_store.root_page_number == 60
+
+    index = cim.Index(repo.cim_type, repo.logical_index_store)
+
+    # collected empirically
+    #
+    # this should be the number of all objects in the repository
+    assert len(index.lookup_keys(cim.Key('NS_'))) == 47490
+
