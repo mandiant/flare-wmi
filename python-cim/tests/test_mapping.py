62defd2609816d90c705d1580f583e1dbf2005c4
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-01 12:28:45 -0400

    tests: add win7 mapping test

---
+++ python-cim/tests/test_mapping.py
@@ -0,0 +1,51 @@
+from fixtures import *
+
+import cim
+
+
+def test_index_mapping(repo):
+    '''
+    Args:
+        repo (cim.CIM): the deleted-instance repo
+
+    Returns:
+        None
+    '''
+    mapping = repo.index_mapping
+
+    # collected empirically.
+    assert len(mapping.entries) == 7824
+    assert mapping.free_dword_count == 241
+    assert mapping.header.physical_page_count == 547
+    assert mapping.header.mapping_entry_count == 326
+
+    assert mapping.get_physical_page_number(logical_page_number=0) == 13
+    assert mapping.get_logical_page_number(physical_page_number=13) == 0
+
+
+def test_index_mapping_inconsistencies(repo):
+    '''
+    find logical pages where the physical page does not map back to it.
+    this is probably where there are two logical pages that point to the
+      same physical page.
+    maybe this is unallocated/deleted regions?
+    
+    Args:
+        repo (cim.CIM): the deleted-instance repo
+
+    Returns:
+        None
+    '''
+
+    mapping = repo.index_mapping
+
+    # logical pages where the physical page does not map back to it.
+    # that is, there must be two logical pages that point here.
+    inconsistencies = []
+    for i in range(mapping.header.mapping_entry_count):
+        pnum = mapping.get_physical_page_number(logical_page_number=i)
+        if i != mapping.get_logical_page_number(physical_page_number=pnum):
+            inconsistencies.append(i)
+
+    assert inconsistencies == [91, 160, 201, 202, 203, 204, 205, 206, 207, 208,
+                               209, 210, 211, 212, 213, 214, 215, 227, 228]
