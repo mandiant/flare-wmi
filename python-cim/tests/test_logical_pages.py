e116564f584d5941b537d2e92ca03df351a1e1d3
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-01 14:58:47 -0400

    tests: add test demonstrating toc is sorted

--- python-cim/tests/test_logical_pages.py
+++ python-cim/tests/test_logical_pages.py
@@ -22,8 +22,8 @@ def test_basic_data_page(repo):
 
     page = datapages.get_page(0x0)
     # collected empirically
-    assert page.toc.count == 23
-    assert len(page.objects) == 23
+    assert page.toc.count == 22
+    assert len(page.objects) == 22
 
     for i in range(repo.data_mapping.header.mapping_entry_count):
         if not repo.data_mapping.is_logical_page_mapped(i):
@@ -33,3 +33,26 @@ def test_basic_data_page(repo):
         assert p.toc.count == len(p.objects)
 
 
+def test_toc_is_ascending(repo):
+    '''
+    demonstrate that the entries in a logical data page TOC are ascending by offset.
+    
+    Args:
+        repo (cim.CIM): the deleted-instance repo
+
+    Returns:
+        None
+    '''
+
+    datapages = repo.logical_data_store
+
+    for i in range(repo.data_mapping.header.mapping_entry_count):
+        if not repo.data_mapping.is_logical_page_mapped(i):
+            continue
+
+        page = datapages.get_page(i)
+        last_offset = 0
+        for j in range(page.toc.count):
+            entry = page.toc[j]
+            assert entry.offset > last_offset
+            last_offset = entry.offset
