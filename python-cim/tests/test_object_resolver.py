e861aaaed3f8e6cd9cb7f872e24282810d0adf62
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-02 10:09:43 -0400

    tests: add test for object resolver fetching all objects in index

---
+++ python-cim/tests/test_object_resolver.py
@@ -0,0 +1,22 @@
+import cim
+import cim.objects
+from fixtures import *
+
+
+def test_object_resolver(repo):
+    '''
+    Args:
+        repo (cim.CIM): the deleted-instance repo
+
+    Returns:
+        None
+    '''
+    resolver = cim.objects.ObjectResolver(repo)
+
+    assert len(resolver.get_keys(cim.Key('NS_'))) == 47490
+
+    for key in resolver.get_keys(cim.Key('NS_')):
+        if not key.is_data_reference:
+            continue
+        o = resolver.get_object(key)
+        assert o is not None
