c17d1c4443b1842825ea272cc3c9852e759ec9e2
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-04 12:47:32 -0400

    dump_page_slack: use recovery utility routines

--- python-cim/samples/dump_page_slack.py
+++ python-cim/samples/dump_page_slack.py
@@ -10,9 +10,9 @@ import sys
 import logging
 
 import argparse
-import intervaltree
 
 import cim
+import cim.recovery
 
 
 logger = logging.getLogger(__name__)
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 def extract_data_page_slack(repo):
     for i in range(repo.logical_data_store.page_count):
-        if not repo.data_mapping.is_physical_page_mapped(i):
+        if not repo.data_mapping.is_logical_page_mapped(i):
             continue
 
         try:
@@ -28,32 +28,10 @@ def extract_data_page_slack(repo):
         except IndexError:
             break
 
-        # start by marking the entire page as allocated
-        slack = intervaltree.IntervalTree([intervaltree.Interval(0, cim.DATA_PAGE_SIZE)])
-
-        # remove the toc region
-        slack.chop(0, len(page.toc))
-
-        # if there is a toc, then we remove the empty entry at the end
-        # (this is not included in the list of entries, but its part of the toc).
-        if len(page.toc) > 0:
-            slack.chop(len(page.toc), len(page.toc) + 0x10)
-
-        # and regions for each of the entries
-        for j in range(page.toc.count):
-            entry = page.toc[j]
-            slack.chop(entry.offset, entry.offset + entry.size)
-
-        pagebuf = repo.logical_data_store.get_logical_page_buffer(i)
-
-        for region in sorted(slack):
-            begin, end, _ = region
-            if (end - begin) > cim.DATA_PAGE_SIZE:
-                continue
-
+        for region in cim.recovery.extract_data_page_slack(page):
             logger.info('extracted %s bytes of slack from logical page %s at offset %s',
-                        hex(end - begin), hex(i), hex(begin))
-            yield pagebuf[begin:end]
+                        hex(len(region.buffer)), hex(i), hex(region.page_offset))
+            yield region.buffer
 
 
 def main(argv=None):
