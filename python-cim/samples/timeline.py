d7e959115473a670bba704dd4c63b73825b039b2
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-08 11:01:22 -0400

    samples: timeline: update and make nice

--- python-cim/samples/timeline.py
+++ python-cim/samples/timeline.py
@@ -1,45 +1,99 @@
+#!/usr/bin/env python2
+"""
+timeline metadata from a WMI repository.
+
+author: Willi Ballenthin
+email: william.ballenthin@fireeye.com
+"""
+import sys
 import logging
-import traceback
+import collections
+
+import argparse
+
+import cim
+import cim.objects
 
 
-from cim import CIM
-from cim.objects import Tree
-
-
-logger = logging.getLogger("cim.timeline")
+logger = logging.getLogger(__name__)
 
 
 def format_ts(ts):
+    '''
+    format a timestamp object nicely.
+    
+    Args:
+        ts (datetime.datetime): the timestamp instance.
+
+    Returns:
+        str: the formatted timestamp string.
+    '''
     return ts.isoformat("T") + "Z"
 
 
-def rec_namespace(namespace):
-    for klass in namespace.classes:
-        print("{ts:s},ClassDefinition.timestamp,{id:s}".format(
-            ts=format_ts(klass.cd.header.timestamp), id=repr(klass)))
-        for instance in klass.instances:
-            try:
-                print("{ts:s},ClassInstance.timestamp1,{id:s}".format(
-                    ts=format_ts(instance.ci.ts1), id=repr(instance)))
-                print("{ts:s},ClassInstance.timestamp2,{id:s}".format(
-                    ts=format_ts(instance.ci.ts2), id=repr(instance)))
-            except:
-                logger.error(traceback.format_exc())
-    for ns in namespace.namespaces:
-        rec_namespace(ns)
+TimelineEntry = collections.namedtuple("TimelineEntry", ["ts", "label", "key"])
 
 
-def main(type_, path):
-    if type_ not in ("xp", "win7"):
-        raise RuntimeError("Invalid mapping type: {:s}".format(type_))
+def timeline(repo):
+    '''
+    extract the timestamps from a wmi repository into an ordered list.
+    
+    Args:
+        repo (cim.CIM): the repository form which to extract timestamps.
 
-    c = CIM(type_, path)
-    tree = Tree(c)
-    rec_namespace(tree.root)
+    Returns:
+        List[TimelineEntry]: the timeline of timestamps in the repository.
+    '''
+    entries = []
+    def rec(namespace):
+        for klass in namespace.classes:
+            entries.append(TimelineEntry(ts=klass.cd.header.timestamp,
+                                         label="ClassDefinition.timestamp",
+                                         key=str(klass)))
+            for instance in klass.instances:
+                entries.append(TimelineEntry(ts=instance.ci.ts1,
+                                             label="ClassInstance.timestamp1",
+                                             key=str(instance)))
+                entries.append(TimelineEntry(ts=instance.ci.ts2,
+                                             label="ClassInstance.timestamp2",
+                                             key=str(instance)))
+        for ns in namespace.namespaces:
+            rec(ns)
+
+    tree = cim.objects.Tree(repo)
+    rec(tree.root)
+    return sorted(entries, key=lambda e: e.ts)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description="Timeline metadata in a WMI repository.")
+    parser.add_argument("input", type=str,
+                        help="Path to input file")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Enable debug logging")
+    parser.add_argument("-q", "--quiet", action="store_true",
+                        help="Disable all output but errors")
+    args = parser.parse_args(args=argv)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger().setLevel(logging.DEBUG)
+    elif args.quiet:
+        logging.basicConfig(level=logging.ERROR)
+        logging.getLogger().setLevel(logging.ERROR)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger().setLevel(logging.INFO)
+
+    repo = cim.CIM.from_path(args.input)
+    for entry in timeline(repo):
+        print('{ts:s}\t{entry.label:s}\t{entry.key:s}'.format(entry=entry, ts=format_ts(entry.ts)))
+    return 0
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    import sys
-
-    main(*sys.argv[1:])
+    main(sys.argv[1:])
